d6c0373e07a036af78d0b28909d93375701052e5
amevide998 <hdsvidi@gmail.com>
2023-10-04 19:54:00 +0700

    adding tags doc

--- blog/main.py
+++ blog/main.py
@@ -15,7 +15,7 @@ def get_db():
     finally:
         db.close()
         
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blog'])
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, description=request.description)
     db.add(new_blog)
@@ -23,12 +23,12 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog', response_model=List[schemas.ShowBlog])
+@app.get('/blog', response_model=List[schemas.ShowBlog],tags=['blog'])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog,tags=['blog'])
 def getById(id, db: Session = Depends(get_db), response: Response = 200):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -38,13 +38,13 @@ def getById(id, db: Session = Depends(get_db), response: Response = 200):
     return blog
 
 
-@app.delete(path='/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete(path='/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blog'])
 def destroy(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
     db.commit()
     return {'detail': f'blog with id {id} deleted'}
 
-@app.put(path='/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put(path='/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blog'])
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -55,7 +55,7 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 # USER 
-@app.post(path='/user', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
+@app.post(path='/user', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser, tags=['user'])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(username=request.username, email=request.email, password=hashing.bcrypt(request.password))
     db.add(new_user)
@@ -63,7 +63,7 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@app.get('/user/{id}', response_model=schemas.ShowUser)
+@app.get('/user/{id}', response_model=schemas.ShowUser, tags=['user'])
 def get_user_by_id(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
